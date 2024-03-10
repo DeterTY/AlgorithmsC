6b5d21c671d047373c55bd15a05b90a25a72232e
DeterTY <159604957+DeterTY@users.noreply.github.com>
2024-03-10 15:29:16 +1000

    Update LabA3.c

--- LabA3.c
+++ LabA3.c
@@ -3,30 +3,36 @@
 
 int n;
 
-double si(double x)
+// Функция для подынтегральной функции sqrt(Ci(x/3))
+double Ci(double x)
 {
-    return sin(x)/x;
+    return sqrt(cos(x/3));
 }
 
+// Функция для вычисления определенного интеграла методом трапеций
 double trapezoidal_rule(double z, double eps)
 {
     n = 10;
     double prev_result = 0;
     while (1)
     {
-        double h = z/n;
+        double h = z / n;
         double result = 0;
+        
         for(int i = 1; i < n; i++)
         {
             double x = i * h;
-            result += sin(x) * sqrt(x);
+            result += Ci(x) * sqrt(x);
         }
-        result += (sin(0) * sqrt(0) + sin(z) * sqrt(x))/2;
+        
+        result += (Ci(0) * sqrt(0) + Ci(z) * sqrt(z)) / 2;
         result *= h;
+        
         if (fabs(result - prev_result) < eps)
         {
             return result;
         }
+        
         prev_result = result;
         n *= 2;
     }
@@ -37,7 +43,7 @@ int main()
     double start = 0;
     double end = 10;
     double step = 0.2;
-    double eps = 1e-8;
+    double eps = 1e-7;
     printf("z\t\tintegral\n");
     printf("\n");
     for(double z = start; z <= end; z += step)
